3e15f7d192128b8c40f8c9907bb8f9fbfdc46f98
Pedro Vieira <pedro@vieira.net>
2020-07-27 10:48:14 -0300

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -50,14 +50,14 @@ defmodule UeberauthAdfs.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:earmark, "~> 1.2", only: :dev, runtime: false},
-      {:ex_doc, "~> 0.16", only: :dev, runtime: false},
-      {:httpoison, "~> 0.13"},
-      {:joken, "~> 1.5"},
-      {:mock, "~> 0.3.0", only: :test},
-      {:oauth2, "~> 0.9"},
-      {:ueberauth, "~> 0.5"},
-      {:poison, "~> 3.0", optional: true}
+      {:earmark, ">= 1.2.0", only: :dev, runtime: false},
+      {:ex_doc, ">= 0.16.0", only: :dev, runtime: false},
+      {:httpoison, "~> 0.13.0"},
+      {:joken, ">= 1.5.0"},
+      {:mock, ">= 0.3.0", only: :test},
+      {:oauth2, ">= 0.9.0"},
+      {:ueberauth, ">= 0.5.0"},
+      {:poison, ">= 3.0.0", optional: true}
     ]
   end
 end
